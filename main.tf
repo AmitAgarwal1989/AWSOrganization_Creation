f60693e94c63334fd2e98f8f69d213b27cac4bb9
Amit Agarwal <1989amit@gmail.com>
2022-12-22 11:03:46 +0530

    Added SCP Policy for Region Restrictions

--- main.tf
+++ main.tf
@@ -41,3 +41,31 @@ resource "aws_organizations_account" "ou_account" {
 
   parent_id = element(aws_organizations_organizational_unit.OU[*].id,count.index)
 }
+data "aws_iam_policy_document" "restrict_regions" {
+  statement {
+    sid       = "RegionRestriction"
+    effect    = "Deny"
+    actions   = ["*"]
+    resources = ["*"]
+
+    condition {
+      test     = "StringNotEquals"
+      variable = "aws:RequestedRegion"
+
+      values = [
+        "us-east-1"
+      ]
+    }
+  }
+}
+
+resource "aws_organizations_policy" "restrict_regions" {
+  name        = "restrict_regions"
+  description = "Deny all regions except US East 1."
+  content     = data.aws_iam_policy_document.restrict_regions.json
+}
+
+resource "aws_organizations_policy_attachment" "restrict_regions_on_root" {
+  policy_id = aws_organizations_policy.restrict_regions.id
+  target_id = aws_organizations_organization.my_org.roots[0].id
+}
